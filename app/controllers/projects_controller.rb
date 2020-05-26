c2fda50a409934f62054e8d84066a658d4d1613c
Subham Sarkar <subham.sarkar123@gmail.com>
2020-05-27 02:05:26 +0530

    Refactoring of project controller is done

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,11 +1,19 @@
 class ProjectsController < ApplicationController
     before_action :authorized, only: [:create, :new, :show, :edit, :update]
-    before_action :get_user
-       
-    def create
+    before_action :get_user, except: [:createfeature]
+    before_action :get_project, only: [:show]
+      
+    def get_user
         @user = User.find(params[:user_id])
+    end 
+    
+    def get_project
+        @project = @user.projects.find(params[:id])
+    end    
+    
+    
+    def create
         @project = @user.projects.build(post_params)
-
         if @project.save
             session[:project_id] = @project.id
             redirect_to user_projects_profile_path, success: "Projects is created"
@@ -16,8 +24,6 @@ class ProjectsController < ApplicationController
 
     def show
         @user_id = current_user.id
-        @user = User.find(params[:user_id])
-        @project = @user.projects.find(params[:id])
         session[:project_id] = @project.id
     end   
 
@@ -27,18 +33,15 @@ class ProjectsController < ApplicationController
     end    
     
     def projects
-        @user = User.find(params[:user_id])
     end   
 
     def edit
         @user_id = current_user.id
-        @user = User.find(params[:user_id])
         @project = Project.find(params[:id])
     end    
     
     def update
         @user_id = current_user.id
-        @user = User.find(params[:user_id])
         @project = Project.find(params[:id])
         if @project.update(post_params)
             redirect_to user_projects_profile_path, success: "Projects is updated"     
